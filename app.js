b88c531a829bfd58052349349bebdb8c4ec28e8a
rage5181 <ragew@naver.com>
2025-05-10 22:31:42 +0900

    fix:use dynamic port for deployment

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/api/groups', groupRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/schedules', scheduleRoutes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
