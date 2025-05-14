22013fdc86b74affe8037cd296cd14d44e6e64d6
rage5181 <ragew@naver.com>
2025-05-15 08:25:02 +0900

    modify db.js

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+/*
 const dotenv = require('dotenv');
 
-// NODE_ENV가 production이면 .env.production을 로드
+
 dotenv.config({
   path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
 });
+*/
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
