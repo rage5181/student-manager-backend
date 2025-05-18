dd73fc5ff2a2ec66eb6aeb43f610fc7304f99f1b
rage5181 <ragew@naver.com>
2025-05-18 16:29:35 +0900

    modify sql env info db.js

--- db.js
+++ db.js
@@ -1,19 +1,16 @@
 const mysql = require('mysql2');
-/*
-const dotenv = require('dotenv');
 
-
-dotenv.config({
-  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
-});
-*/
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+  console.log("✅ .env 로드 완료 (개발 환경)");
+}
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT, 
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT, 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
